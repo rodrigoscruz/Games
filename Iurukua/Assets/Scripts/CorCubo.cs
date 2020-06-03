dad2f633767a42051e731145deaabd9206794e9f
Rodrick <rs.strat@gmail.com>
2020-06-02 22:18:23 -0300

    Primeiro game 3D

---
+++ Iurukua/Assets/Scripts/CorCubo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorCubo : MonoBehaviour
+{
+    public Color corInicial = Color.white;
+    Material materialObjeto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        materialObjeto = GetComponent<MeshRenderer>().material;
+        materialObjeto.color = corInicial;
+    }
+
+    public void Azul()
+    {
+        corInicial = Color.blue;
+
+        materialObjeto = GetComponent<MeshRenderer>().material;
+        materialObjeto.color = corInicial;
+    }
+
+    public void Vermelho()
+    {
+        corInicial = Color.red;
+
+        materialObjeto = GetComponent<MeshRenderer>().material;
+        materialObjeto.color = corInicial;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
